144002f792d46849f307f1f919fd6ed8718e0e0a
EstebanMiranda08 <es.miranda.ahumada@gmail.com>
2025-03-02 22:22:39 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 
-let amigos = []
+let amigos = [martin, pedro, miguel, fernando];
+
